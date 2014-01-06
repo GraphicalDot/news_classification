77fe570e603579e3e74b494b8763f90ac8b00efd
root <root@kaali.(none)>
2014-01-07 00:50:13 +0530

    rss feeds files updated for last epoch

--- rss_feeds.py
+++ rss_feeds.py
@@ -5,6 +5,7 @@ from variables import HINDUSTAN_TIMES_NDLS, TOI_NDLS, HINDU_NDLS, DB
 from database import collection
 from  datetime import datetime
 import time
+import sys
 
 
 def updating_epoch_collection(src_name, epoch):
@@ -13,10 +14,18 @@ def updating_epoch_collection(src_name, epoch):
 	as the upsert flag is set to be True.
 
 	"""
-	collection = collection("Epoch")
-	collection.update({"name": src_name},{"$set": {"epoch": epoch}}, upsert = True)
+	mongo_collection = collection("Epoch")
+	mongo_collection.update({"name": src_name},{"$set": {"epoch": epoch}}, upsert = True)
 	sys.exit(1)
 
+
+def get_last_epoch(src_name):
+	mongo_collection = collection("Epoch")
+	try:
+		return mongo_collection.find_one({"name": src_name}).get("epoch")
+
+	except AttributeError:
+		return None
 
 def converting_to_epoch(time_struct_object):
 	"""
@@ -40,9 +49,17 @@ def hindustan_ndls_rss():
 	5.published date converted into epoch time(which will be later used to identify the news already present in our 
 		database.
 
+	variables:
+		data_sort = This is the variable which will have the sorted data list on the basis of the epoch times 
+		which ae stored under the name published_date
 	"""
 	rss_data = feedparser.parse(HINDUSTAN_TIMES_NDLS)
-	return [dict(link= rss.get("link"), published=rss.get("published"), summary=rss.get("summary"), headline=rss.get("title"), published_date= converting_to_epoch(rss.get("published_parsed")), ) for rss in rss_data.get("entries")]
+	data =  [dict(link= rss.get("link"), published=rss.get("published"), summary=rss.get("summary"), headline=rss.get("title"), published_date= converting_to_epoch(rss.get("published_parsed")), ) for rss in rss_data.get("entries")]
+	data_sort = sorted(data, key=lambda x: x["published_date"], reverse=True)
+	updating_epoch_collection("HT_NDLS", data_sort[0].get("published_date"))
+	return data_sort
+
+
 
 def hindu_ndls_rss():
 	"""
@@ -55,10 +72,16 @@ def hindu_ndls_rss():
 	4.title of the news
 	5.published date converted into epoch time(which will be later used to identify the news already present in our 
 		database.
+	variables:
+		data_sort = This is the variable which will have the sorted data list on the basis of the epoch times 
+		which ae stored under the name published_date
 
 	"""
 	rss_data = feedparser.parse(HINDU_NDLS)
-	return [dict(link= rss.get("link"), published=rss.get("published"), summary=rss.get("summary"), headline=rss.get("title"), published_date= converting_to_epoch(rss.get("published_parsed")), ) for rss in rss_data.get("entries")]
+	data = [dict(link= rss.get("link"), published=rss.get("published"), summary=rss.get("summary"), headline=rss.get("title"), published_date= converting_to_epoch(rss.get("published_parsed")), ) for rss in rss_data.get("entries")]
+	data_sort = sorted(data, key=lambda x: x["published_date"], reverse=True)
+	updating_epoch_collection("HIN_NDLS", data_sort[0].get("published_date"))
+	return data_sort
 
 
 
@@ -73,11 +96,21 @@ def toi_ndls_rss():
 	5.published date converted into epoch time(which will be later used to identify the news already present in our 
 		database.
 
+	variables:
+		data_sort = This is the variable which will have the sorted data list on the basis of the epoch times 
+		which ae stored under the name published_date
 	"""
 
 	rss_data = feedparser.parse(HINDUSTAN_TIMES_NDLS)
-	return [dict(link= rss.get("link"), published=rss.get("published"), summary=rss.get("summary"), headline=rss.get("title"), published_date= converting_to_epoch(rss.get("published_parsed")),  ) for rss in rss_data.get("entries")]
+	data =  [dict(link= rss.get("link"), published=rss.get("published"), summary=rss.get("summary"), headline=rss.get("title"), published_date= converting_to_epoch(rss.get("published_parsed")),  ) for rss in rss_data.get("entries")]
+	data_sort = sorted(data, key=lambda x: x["published_date"], reverse=True)
+	updated = [item for item in data_sort if item.get("published_date") > get_last_epoch("TOI_NDLS")]
+	print updated
+	if updated:
+		updating_epoch_collection("TOI_NDLS", updated[0].get("published_date"))
+	return updated
 
 
-
+if __name__ == "__main__":
+	toi_ndls_rss()
 
