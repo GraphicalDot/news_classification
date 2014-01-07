ff38dfe22e74cc844a4a8e852d339e7be1f84813
root <root@kaali.(none)>
2014-01-08 00:41:03 +0530

    fabric file updated but still incomplete

--- fabfile.py
+++ fabfile.py
@@ -24,6 +24,8 @@ def AD_virtuaEnv():
 	run("sudo apt-get install -y python-all-dev")
 	run("sudo apt-get install -y python-pip")
 	run("sudo apt-get install -y ipython")
+	run("sudo apt-get install -y python-setuptools python-dev build-essential")
+
 
 def virtual_env():
 	run("sudo pip install virtualenv")
@@ -33,7 +35,11 @@ def virtual_env():
 			run("sudo apt-get install -y git")
 			with prefix("source bin/activate"):
 				run("git clone https://github.com/kaali-python/news_classification.git")
-				run("ls")
+
+
+def installing_requirements():
+	with prefix("cd /home/ubuntu/VirtualEnvironment &&source bin/activate && cd news_classification"):
+		run("sudo pip install -r requirements.txt")
 
 def updating_git():
 	with prefix("cd /home/ubuntu/VirtualEnvironment &&source bin/activate && cd news_classification"):
@@ -45,19 +51,58 @@ def installation_script():
 		run("./installation.sh")
 
 
-def deploy():
-	print(_green("Connecting to EC2 Instance..."))	
-	with hide("warnings"):
-	#	execute(AD_virtuaEnv)
-		execute(installation_script)
-		print(_yellow("...Disconnecting EC2 instance..."))
-		disconnect_all()
+
+def nginx():
+	"""
+	This function installs nginx on the remote server and replaces its conf file with the one available in the
+	git repository.Finally restart the nginx server
+	"""
+	with prefix("cd /home/ubuntu/VirtualEnvironment &&source bin/activate && cd news_classification"):
+		run("sudo apt-get install -y nginx")
+	execute(update_nginx_conf)
+	run("sudo service nginx restart")
+
+
+
+def update_nginx_conf():
+	with prefix("cd /home/ubuntu/VirtualEnvironment &&source bin/activate && cd news_classification/configs"):
+		run("sudo cp nginx.conf /etc/nginx/sites-enabled/default")
+
+
+
+
+def mongo():
+	with prefix("cd /home/ubuntu/VirtualEnvironment &&source bin/activate && cd news_classification"):
+		run("sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10")
+		run("echo -e 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list")
+		run("sudo apt-get update")
+		run("sudo apt-get install -y mongodb-10gen")
+	execute(update_mongo_conf)
+	run("sudo service mongodb restart")
+
+def update_mongo_conf():
+	with prefix("cd /home/ubuntu/VirtualEnvironment &&source bin/activate && cd news_classification/configs"):
+		run("sudo cp mongodb.conf /etc/mongodb.conf")
+
+
+
+def supervisor():
+	pass
 
 def update():
 	print(_green("Connecting to EC2 Instance..."))	
 	with hide("warnings"):
 		execute(updating_git)
-		execute(installation_script)
+		print(_yellow("...Disconnecting EC2 instance..."))
+		disconnect_all()
+
+
+def deploy():
+	print(_green("Connecting to EC2 Instance..."))	
+	with hide("warnings"):
+	#	execute(AD_virtuaEnv)
+	#	execute(installing_requirements)
+		execute(mongo)
 		print(_yellow("...Disconnecting EC2 instance..."))
 		disconnect_all()
 
--- installation.sh
+++ installation.sh
@@ -4,39 +4,6 @@
 echo -e "\n\nMaking application logs directory which will have the related logs for all the applications running\n\n"
 sudo mkdir /applogs
 
-echo -e "\n\nInstalling python easy_install tool\n\n"
-sudo apt-get install -y python-setuptools python-dev build-essential
-
-
-echo -e "\n\nInstalling python devlopers tools\n\n"
-sudo easy_install greenlet
-sudo easy_install gevent
-
-
-echo -e "\n\nInstalling flask\n\n"
-sudo pip install Flask
-
-
-echo -e "\n\nInstalling mongodb\n\n"
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo -e 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-sudo apt-get update
-sudo apt-get install -y mongodb-10gen
-
-echo -e "Installing python application interface for using mongodb"
-pip install pymongo
-
-
-echo -e  "\n\nInstalling python reqeusts module to use http protocols\n\n"
-pip install requests
-
-echo -e "\n\nInstalling flask-restful for using http standard protocols to be used in python\n\n"
-pip install flask-restful
-
-
-echo -e "\n\nInstalling gunicorn and its dependicies\n\n"
-pip install gunicorn
-
 echo -e "\n\nInstalling supervisord  and its changing its configuration file for gunicorn\n\n"
 pip install supervisor
 echo_supervisord_conf > /etc/supervisord.conf
