bb64edaae7186587623434e1c18c9159217deefa
root <root@kaali.(none)>
2014-01-08 14:08:08 +0530

    fabfile still incomplete

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,7 @@ from __future__ import with_statement
 from fabric.api import local, settings, prefix, abort, run, cd, env, require, hide, execute
 from fabric.contrib.console import confirm
 from fabric.network import disconnect_all
-from fabric.colors import green as _green, yellow as _yellow
+from fabric.colors import green as _green, yellow as _yellow, red as _red
 from fabric.contrib.files import exists
 import os
 
@@ -19,11 +19,20 @@ This is the file which remotely makes an ec2 instance for the use of this reposi
 """
 
 def before_env():
+	""""
+	This method should be run before installing virtual environment as it will install python pip
+	required to install virtual environment
+
+	"""
 	run("sudo apt-get upgrade")
 	run("sudo apt-get update")
 	run("sudo apt-get install -y python-pip")
 
 def after_env():
+	"""
+	This method activates the virtual environment and in virtual environment installs the required dependicies
+	and should be run after installing virtual environement
+	"""
 	with prefix("cd /home/ubuntu/VirtualEnvironment &&source bin/activate && cd news_classification"):
 		run("sudo apt-get install -y libevent-dev")
 		run("sudo apt-get install -y python-all-dev")
@@ -32,6 +41,11 @@ def after_env():
 
 
 def virtual_env():
+	"""
+	This method installs the virual environment and after installing virtual environment installs the git.
+	After installing the git installs the reuiqred repository
+	"""
+
 	run("sudo pip install virtualenv")
 	with cd("/home/ubuntu/"):
 		run("virtualenv --no-site-packages VirtualEnvironment")
@@ -45,10 +59,17 @@ def virtual_env():
 
 
 def installing_requirements():
+	"""
+	This function installs all the requirements required to run the package with the help of requirements.txt
+	"""
 	with prefix("cd /home/ubuntu/VirtualEnvironment &&source bin/activate && cd news_classification"):
 		run("sudo pip install -r requirements.txt")
 
-def updating_git():
+def update_git():
+	"""
+	This method will be run everytime the git repository is updated on the main machine.This clones the pushed updated 
+	repository on the git on the remote server
+	"""
 	with prefix("cd /home/ubuntu/VirtualEnvironment &&source bin/activate && cd news_classification"):
 		run("git pull origin master")
 
@@ -77,6 +98,11 @@ def update_nginx_conf():
 
 
 def mongo():
+	"""
+	This method installs the mongodb database on the remote server.It after installing the mongodb replaces the 
+	mongodb configuration with the one available in the git repository.
+
+	"""
 	with prefix("cd /home/ubuntu/VirtualEnvironment &&source bin/activate && cd news_classification"):
 		run("sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10")
 		run("echo -e 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list")
@@ -84,18 +110,32 @@ def mongo():
 		run("sudo apt-get install -y mongodb-10gen")
 	execute(update_mongo_conf)
 
+
 def update_mongo_conf():
+	"""
+	This method updates the mongodb configuration file available in the git repository.It after replacement, restarts
+	the mongodb engine to update the configuration file.
+
+	"""
 	with prefix("cd /home/ubuntu/VirtualEnvironment &&source bin/activate && cd news_classification/configs"):
 		run("sudo cp mongodb.conf /etc/mongodb.conf")
 		run("sudo service mongodb restart")
 	
 
 def supervisord():
+	"""
+	It updates the supervisord configuration file which has already been installed by the pip -r requirements.txt
+	"""
 	with prefix("cd /home/ubuntu/VirtualEnvironment &&source bin/activate && cd news_classification"):
 		run("sudo echo_supervisord_conf > /etc/supervisord.conf")
 	execute("update_supervisord_conf")
 
 def update_supervisord_conf():
+	"""
+	This method updates the supervisord configuration file available in the git repository.It then restarts the 
+	supervisorctl to update the configuration file.
+
+	"""
 	with prefix("cd /home/ubuntu/VirtualEnvironment &&source bin/activate && cd news_classification/configs"):
 		run("sudo cp supervisord.conf /etc/supervisord.conf")
 		run("sudo supervisorctl restart")
@@ -110,7 +150,7 @@ def supervisorctl(process):
 
 def supervisord_status():
 	"""
-	This method outputs the status of the process being run by supervisord
+	This method outputs the status of the process being run by supervisord on the remote server.
 	"""
 	print(_green("Getting status of the process running through supervisord..."))	
 	with hide("warnings"):
@@ -118,12 +158,58 @@ def supervisord_status():
 			run("sudo supervisorctl status")
 		disconnect_all()
 
+
+def nginx_status():
+	    """
+	    Check if nginx is installed.
+	    """
+	    with settings(hide("running", "stdout", "stderr"), warn_only=True):
+	    	result = run('if ps aux | grep -v grep | grep -i "nginx"; then echo 1; else echo ""; fi')
+
+	    	if result == 1:
+			    print (_green("Nginx is running fine......................"))
+	    	else:
+			    print (_red("Nginx is not running ......................"))
+
+
+def gunicorn_status():
+	"""
+	Check if gunicorn is running or not
+	"""
+	with settings(hide("running", "stdout", "stderr"), warn_only=True):
+		result = run('if ps aux | grep -v grep | grep -i "gunicorn"; then echo 1; else echo ""; fi')
+		if result == 1:
+			print (_green("Gunicorn is running fine......................"))
+		else:
+			print (_red("Gunicorn is not running ......................"))
+
+
+
+def status():
+	print(_green("Connecting to EC2 Instance..."))	
+	with hide("warnings"):
+		execute(supervisord_status)
+		execute(nginx_status)
+		execute(gunicorn_status)
+		run("sudo nginx -t")
+		print(_yellow("...Disconnecting EC2 instance..."))
+		disconnect_all()
+
+
+
 def update():
 	print(_green("Connecting to EC2 Instance..."))	
 	with hide("warnings"):
-		execute(updating_git)
+		execute(update_git)
+		execute(update_nginx_conf)
+		execute(update_supervisord_conf)
+		execute(nginx_status)
+		execute(gunicorn_status)
+		
 		print(_yellow("...Disconnecting EC2 instance..."))
 		disconnect_all()
+
+
 
 
 def deploy():
